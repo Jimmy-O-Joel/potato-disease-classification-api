8da8b3bcd8180370ef6302b92cba51a04a87579c
Jimmy Joel <jimmyjoel177@gmail.com>
2023-01-25 22:51:03 +0300

    added a doployed server

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ app = FastAPI()
 origins = [
     "http://localhost",
     "http://localhost:3000",
+    "https://potato-disease-classification.netlify.app/"
 ]
 app.add_middleware(
     CORSMiddleware,
